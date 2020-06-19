85694e77f6b08ffa580f586bf365308e2788e965
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2020-06-18 23:09:38 -0300

    Adicionado a ckasse Utils para formatar a data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,6 +9,9 @@ class UserController {
 
         this.formEl.addEventListener("submit", event => {
             event.preventDefault();
+
+            let btn = this.formEl.querySelector("[type = submit]");
+            btn.disabled = true;
             //esse trexo faz a leitura da foto
             let values = this.getValues();
             
@@ -16,6 +19,10 @@ class UserController {
                 (content)=>{
                     values.photo = content;
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
                 },
                 (e)=>{
                     console.error(e);
@@ -44,7 +51,12 @@ class UserController {
                 reject(e);
             };
 
-            fileReader.readAsDataURL(file);
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+            
         });
         
     }
@@ -59,6 +71,8 @@ class UserController {
                     user[field.name] = field.value;
                 }
                 
+            } else if(field.name == "admin") {
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }
@@ -81,19 +95,21 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+        
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
+        
         `;
+        this.tableEl.appendChild(tr);
     
     }
 
